aeccc1885238e1506159a57b104515ae0e0ba76d
aktoriukas <gediminas.strumila@gmail.com>
2020-11-20 19:51:39 +0000

    v1.0

--- src/images/Image.js
+++ src/images/Image.js
@@ -4,10 +4,16 @@ export default class Image extends Component {
     render() {
         return (
             <li>
-                <img src={`./img/${this.props.nr}.jpg`}
-                    onClick={this.props.changeDisplay}
-                    alt={this.props.nr}
-                ></img>
+                <a
+                    href={`#${this.props.nr}`}
+                >
+                    <img 
+                        src={`./img/${this.props.nr}.jpg`}
+                        onClick={this.props.changeDisplay}
+                        alt={this.props.nr}
+                        id={this.props.nr}
+                    ></img>
+                </a>
             </li>
         )
     }
