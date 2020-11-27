5464153e088959ca80485898ce0d8d28960b6d7f
aktoriukas <gediminas.strumila@gmail.com>
2020-11-27 15:54:10 +0000

    display image locig

--- src/images/Image.js
+++ src/images/Image.js
@@ -2,18 +2,18 @@ import React, { Component } from 'react'
 
 export default class Image extends Component {
     render() {
+        let display = this.props.display;
+
         return (
-            <li>
-                <a
-                    href={`#${this.props.nr}`}
-                >
-                    <img 
-                        src={`./img/${this.props.nr}.jpg`}
-                        onClick={this.props.changeDisplay}
-                        alt={this.props.nr}
-                        id={this.props.nr}
-                    ></img>
-                </a>
+            <li
+                style={display}
+            >
+                <img 
+                    src={`./img/${this.props.nr}.jpg`}
+                    onClick={this.props.changeDisplay}
+                    alt={this.props.nr}
+                    id={this.props.nr}
+                ></img>
             </li>
         )
     }
--- src/images/Images.js
+++ src/images/Images.js
@@ -7,29 +7,40 @@ export default class Images extends Component {
         super(props)
     
         this.state = {
-             displayFlex: false
+             displayFlex: false,
+             currentImage: ''
 
         }
         this.changeDisplay = this.changeDisplay.bind(this)
     }
     changeDisplay(e) {
-        console.log(e.target);
+        console.log(e.target.id);
         this.setState({
-            displayFlex: !this.state.displayFlex
+            displayFlex: !this.state.displayFlex,
+            currentImage: e.target.id
         })
     }
     
     render() {
+        const {displayFlex, currentImage} = this.state;
         const imageNumber = 91;
         const imageFinal = [];
+        let displayFormat = '';
 
         for(let i=1; i < imageNumber; i++){
             imageFinal.push(<Image nr={i} key={i}
                 changeDisplay={this.changeDisplay}
+                display={!displayFlex || currentImage == i  ? {display:'inherit'} : {display:'none'}}
             />)
         }
+        if ( displayFlex === false) {
+            displayFormat = `grid`;
+        } else {
+            displayFormat = `flex ${currentImage}`;
+        }
+
         return (
-            <ul className={`images-container ${this.state.displayFlex === false ?'grid' :'flex'}`}>
+            <ul className={`images-container ${displayFormat}`}>
                 {imageFinal}
             </ul>
         )
